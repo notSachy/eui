47368a68de7cd671e7b110212be29ca87beea95f
EllesmereGaming <ellesmeregaming@gmail.com>
2026-03-08 16:10:11 -0400

    Release v3.7.4

--- EllesmereUI.lua
+++ EllesmereUI.lua
@@ -5101,7 +5101,7 @@ end
 -------------------------------------------------------------------------------
 --  Slash commands
 -------------------------------------------------------------------------------
-EllesmereUI.VERSION = "3.7.1"
+EllesmereUI.VERSION = "3.7.4"
 
 -- Register this addon's version into a shared global table (taint-free at load time)
 if not _G._EUI_AddonVersions then _G._EUI_AddonVersions = {} end
